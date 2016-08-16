292173a533975eca3019de951fa479aead854b73
John Tran <tran.john1337@gmail.com>
2016-08-16 10:30:48 -0500

    Delete vali.js
    github is static

--- js/vali.js
+++
@@ -1,6 +0,0 @@
-var frmvalidator = new Validator("contact_form");
-frmvalidator.addValidation("name", "req", "Please provide your name");
-frmvalidator.addValidation("email", "req", "Please provide your email");
-frmvalidator.addValidation("email","email", "Please enter a valid email address");
-frmvalidator.addValidation("subject","req", "Please provide your subject");
-frmvalidator.addValidation("message", "req", "Please provide your thoughts");
